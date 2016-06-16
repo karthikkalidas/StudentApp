b37c85bc41d055cfbd0aba35f8fcecb9f59029cc
Karthik Kalidas <karthik.kalidas@gmail.com>
2016-06-16 12:51:56 +0530

    All Functions Implemented
    Basic Input() Interface
    File Handling Remaining

--- StudentApp.py
+++ StudentApp.py
@@ -1,22 +1,48 @@
 import demjson
+from pprint import pprint
+import sys
+
 
 class Student:
-        def __init__(self,StudentName,StudentPhoneNumber,StudentPercentage):
-                self.StudentName = StudentName
-                self.StudentPhoneNumber = StudentPhoneNumber
-                self.StudentPercentage =StudentPercentage
+        def __init__(self):
+                self.StudentName=input("Enter Student's Name : ")
+                self.StudentPhoneNumber= input("Enter Student's PhoneNumber : ")
+                self.StudentPercentage= input("Enter Student's Percentage : ")
 
         def Read(self):     
                 print(self.StudentName)
                 print(self.StudentPhoneNumber)
                 print(self.StudentPercentage)
 
-Name=input("Enter Student's Name : ")
-PhoneNumber= input("Enter Student's PhoneNumber : ")
-Percentage= input("Enter Student's Percentage : ")
-
-s1=Student(Name,PhoneNumber,Percentage)
+        
+        def __repr__(self):
+                return ('Name : '+self.StudentName+' PhoneNumber : '+self.StudentPhoneNumber+' Percentage : '+self.StudentPercentage)
 
 
+StudentList = []
 
-s1.Read()
+
+def Add():
+        StudentList.append(Student())
+
+def Delete():
+    StudentList.pop(int(input("Enter Index : "))-1)
+
+def Search():     
+    Name=input("Enter the Name : ")
+    for x in StudentList:
+        if x.StudentName == Name:
+            print(x.StudentName)
+            print(x.StudentPhoneNumber)  
+            print(x.StudentPercentage)
+
+Add()
+Search()
+
+
+
+
+#Make a command line interface that asks me (teacher) what I want to do (add a student, remove a student, edit) and then calls the appropriate function
+
+
+
